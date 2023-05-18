b65df230037e77a7f85de2ce7f2607629f2c150e
cbaltaza <balta1_carlos@hotmail.com>
2023-05-18 09:52:34 -0600

    14
    Version Inicial

---
+++ src/main/java/com/example/springbootapachecamelb/SpringbootapachecamelbApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootapachecamelb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapachecamelbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapachecamelbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootapachecamelb/SpringbootapachecamelbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootapachecamelb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapachecamelbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
